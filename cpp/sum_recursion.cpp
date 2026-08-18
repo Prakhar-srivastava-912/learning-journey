1363ad3c3ff13116d696aea7bc898ba6c715cfe5
Prakhar-srivastava-912 <9eprakharsrivastava09@gmail.com>
2026-08-18 19:53:22 +0530

    Add parameterized recursion sum program

---
+++ cpp/sum_recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void sum(int n, int s)
+{
+    if (n == 0)
+    {
+        cout << "Sum = " << s;
+        return;
+    }
+
+    sum(n - 1, s + n);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    sum(n, 0);
+
+    return 0;
+}
